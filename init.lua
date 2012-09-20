d947502dbe2bdce2c9d64074b7a17e5bf9114d1c
Kyle <kyle.kylina@gmail.com>
2012-09-19 21:09:22 -0700

    added comments, renamed spawn_cube to expand_cube, removed color_to_texture, improved match_cubelet

--- init.lua
+++ init.lua
@@ -1,52 +1,67 @@
+
+--if not(closed_door) go_out()
+--unless(closed_door) go_out()
+-- I want an unless stanement.
+
+--Okay, so we're making a Rubik's Cube!
+--Let's start with the basics.
 colors = {
-	'green',
-	'blue',
-	'red',
-	'orange',
-	'white',
-	'yellow',
+	'green',  -- +Y
+	'blue',   -- -Y
+	'red',    -- +X
+	'orange', -- -X
+	'white',  -- +Z
+	'yellow', -- -Z
 }
 
-materials = {}
-textures = {}
-spawntex = {}
-for c = 1, #colors do
-	materials[c] = 'wool:'..colors[c]
-	textures[c] = 'wool_'..colors[c]..'.png'
-	spawntex[c] = textures[c]..'^rubiks_three.png'
-	textures[c] = textures[c]..'^rubiks_outline.png'
+materials = {} --what you craft the spawner with
+textures = {} --what is on the cubelets
+spawntex = {} --what is on the spawner
+for color = 1, #colors do
+	materials[color] = 'wool:'..colors[color]
+	textures[color] = 'wool_'..colors[color]..'.png'
+	spawntex[color] = textures[color]..'^rubiks_three.png'
+	--textures[color] = textures[color]..'^rubiks_outline.png'
 end
 
-function spawn_cube(pos, create)
+function expand_cube(pos, spawn)
 	for x = pos.x-1, pos.x+1 do
 	for y = pos.y-1, pos.y+1 do
 	for z = pos.z-1, pos.z+1 do
-		pos2 = {x=x, y=y, z=z}
-		if create then
-			if not(pos.x==x and pos.y==y and pos.z==z) then
+	pos2 = {x=x, y=y, z=z}
+		if spawn then --create
+			--don't overwrite the spawner
+			if not(pos2.x==pos.x and pos2.y==pos.y and pos2.z==pos.z) then
+				--always starts the same direction
 				name = 'rubiks:cubelet1'
 				minetest.env:add_node(pos2, {name = name})
+				--keep track of center for the purpose of rotating the cube
 				local meta = minetest.env:get_meta(pos2)
 				meta:set_string('cube_center',
 					minetest.pos_to_string(pos)
 				)
 			end
-		else
+		else --delete
 			minetest.env:remove_node(pos2)
 		end
 	end
 	end
 	end
 	if create then
+		--keep a record so you can't get two cubes from one, or something like that
 		local meta = minetest.env:get_meta(pos)
 		meta:set_int('has_spawned', 1)
 	end
 end
 
+--can't make a rubik's cube without the cube
 minetest.register_node('rubiks:cube', {
+	--spawner because I don't get the uv pos yet
 	description  = "Rubik's Cube Spawner",
 	tiles = spawntex,
+	--show green, yellow, red sides to look 3d in inventory
 	inventory_image = minetest.inventorycube(spawntex[1], spawntex[6], spawntex[3]),
+	--want it to be diggable, quickly
 	groups = {crumbly=3},
 	on_punch = function(pos, node, puncher)
 		for x = pos.x-1, pos.x+1 do
@@ -54,6 +69,7 @@ minetest.register_node('rubiks:cube', {
 		for z = pos.z-1, pos.z+1 do
 			if not(pos.x==x and pos.y==y and pos.z==z) then
 				if minetest.env:get_node({x=x, y=y, z=z}).name ~= 'air' then
+					--put it on a pedestal then remove the pedestal
 					minetest.chat_send_player(puncher:get_player_name(), "Clear some space for Rubik's cube to expand")
 					return
 				end
@@ -61,11 +77,12 @@ minetest.register_node('rubiks:cube', {
 		end
 		end
 		end
-		spawn_cube(pos, true)
+		--surrounded by air, so
+		expand_cube(pos, true)
 	end,
 	can_dig = function(pos, digger)
 		--digging the center of a spawned cube yields
-		--two cubes without this
+		--an extra cube without this - don't cheat when flying
 		local meta = minetest.env:get_meta(pos)
 		if meta:get_int('has_spawned') == 1 then
 			return false
@@ -75,17 +92,18 @@ minetest.register_node('rubiks:cube', {
 
 })
 
+--100% wool, need a way to get wool now.
 minetest.register_craft({
 	type = "shapeless",
 	output = "rubiks:cube",
 	recipe = materials,
-	replacements = {
-		{'bucket:bucket_water', 'bucket:bucket_empty'},
-	},
 })
 
+--from the tiles of a node definition, get the actual tiles based on the facedir
 function facedir_to_tiles(tilestocopy, facedir)
+	--copying tables is annoying
 	tiles = {unpack(tilestocopy)}
+	--minus one because an equal facedir doesn't need rotating
 	for f = 0, facedir-1 do
 		--+Y, -Y, +X, -X, +Z, -Z
 		placeholder = tiles[6]
@@ -97,22 +115,24 @@ function facedir_to_tiles(tilestocopy, facedir)
 	return tiles
 end
 
+--I have the colors of two sides, so give me the cubelet that has that
 function match_cubelet(matchtiles)
-	for wallmounted = 1, 6 do
-		--nodetiles = minetest.registered_nodes['rubiks:cubelet'..wallmounted].tiles
-		nodetiles = {unpack(cubelettiles[wallmounted])}
+	--for rotation "axis" one, do
+	for topcolor = 1, 6 do
+	nodetiles = {unpack(cubelettiles[topcolor])}
+	--for rotation "axis" two, do
 	for facedir = 0, 3 do
-		testtiles = facedir_to_tiles(nodetiles, facedir)
-		--print(wallmounted..' '..facedir..' '..dump(testtiles))
-		if
-		(matchtiles[1] == testtiles[1] or matchtiles[1] == nil) and
-		(matchtiles[2] == testtiles[2] or matchtiles[2] == nil) and
-		(matchtiles[3] == testtiles[3] or matchtiles[3] == nil) and
-		(matchtiles[4] == testtiles[4] or matchtiles[4] == nil) and
-		(matchtiles[5] == testtiles[5] or matchtiles[5] == nil) and
-		(matchtiles[6] == testtiles[6] or matchtiles[6] == nil) then
-			print('testtiles '..dump(testtiles))
-			return 'rubiks:cubelet'..wallmounted, facedir
+	testtiles = facedir_to_tiles(nodetiles, facedir)
+		for tile = 1, 6 do
+			--unless it matches or isn't a side that is being checked for a match, continue
+			if not(
+				matchtiles[tile] == testtiles[tile] or
+				matchtiles[tile] == nil
+			) then break
+			elseif tile == 6 then
+				--match found
+				return 'rubiks:cubelet'..topcolor, facedir
+			end
 		end
 	end
 	end
@@ -120,13 +140,10 @@ function match_cubelet(matchtiles)
 end
 
 function rotate_cube(pos, dir, clockwise, all)
-	print(dump(dir))
 	--save cube to rotate without losing data
 	cube = {}
-	for x = -1, 1 do
-		cube[x] = {}
-	for y = -1, 1 do
-		cube[x][y] = {}
+	for x = -1, 1 do cube[x] = {}
+	for y = -1, 1 do cube[x][y] = {}
 	for z = -1, 1 do
 		--read absolute position, save relative position
 		pos2 = {x=pos.x+x, y=pos.y+y, z=pos.z+z}
@@ -138,9 +155,8 @@ function rotate_cube(pos, dir, clockwise, all)
 	end
 	end
 
-	loadpos = {0, 0, 0}
-	axes = {}
 	--what side of the cube will be rotated on what axes
+	loadpos, axes = {0, 0, 0}, {}
 	if dir.x ~= 0 then
 		loadpos[1] = dir.x
 		axes[1] = 3--z
@@ -163,37 +179,29 @@ function rotate_cube(pos, dir, clockwise, all)
 		--still clockwise, just from the opposite perspective
 	end
 
-	for firstaxis = -1, 1 do
-	loadpos[axes[1]] = firstaxis
-	for secondaxis = -1, 1 do
-	loadpos[axes[2]] = secondaxis
+	--start rotating
+	for firstaxis = -1, 1 do loadpos[axes[1]] = firstaxis
+	for secondaxis = -1, 1 do loadpos[axes[2]] = secondaxis
 
 		--don't lose data here either
 		writepos = {unpack(loadpos)}
 
-		--rotate globally
-		writepos[axes[1]] = loadpos[axes[2]]
-		writepos[axes[2]] = loadpos[axes[1]]
-		if clockwise then
-			writepos[axes[2]] = -writepos[axes[2]]
-		else
-			writepos[axes[1]] = -writepos[axes[1]]
-		end
+		--rotate around center of face
+		writepos[axes[1]] = loadpos[axes[2]] * (clockwise and 1 or -1)
+		writepos[axes[2]] = loadpos[axes[1]] * (clockwise and -1 or 1)
 
 		--get absolute position
 		pos2 = {x=pos.x+writepos[1], y=pos.y+writepos[2], z=pos.z+writepos[3]}
 
+		--rotate cubelet itself
 		loadcubelet = cube[loadpos[1]][loadpos[2]][loadpos[3]]
-		--rotate locally
-		--oldtiles = minetest.registered_nodes[loadcubelet.node.name].tiles
-		wallmounted = string.gsub(loadcubelet.node.name, 'rubiks:cubelet', '')
-		oldtiles = {unpack(cubelettiles[wallmounted+0])}
-		--print('oldtiles '..dump(oldtiles))
-		--print('oldparam2 '..dump(loadcubelet.node.param2))
-		oldtiles = facedir_to_tiles(oldtiles, loadcubelet.node.param2)
-		--print('newoldtiles '..dump(oldtiles))
+		topcolor = string.gsub(loadcubelet.node.name, 'rubiks:cubelet', '')
+		oldtiles = facedir_to_tiles(
+			cubelettiles[topcolor+0],
+			loadcubelet.node.param2
+		)
 		matchtiles = {
-			--match the side that is spinning
+			--match the face
 			dir.y ==  1 and oldtiles[1]..'' or nil,
 			dir.y == -1 and oldtiles[2]..'' or nil,
 			dir.x ==  1 and oldtiles[3]..'' or nil,
@@ -201,7 +209,7 @@ function rotate_cube(pos, dir, clockwise, all)
 			dir.z ==  1 and oldtiles[5]..'' or nil,
 			dir.z == -1 and oldtiles[6]..'' or nil,
 		}
-		--match the side that is turning
+		--match a turning side
 		--+Y, -Y, +X, -X, +Z, -Z
 		if dir.x ~= 0 then
 			-- +Y = +Z or -Z
@@ -215,42 +223,21 @@ function rotate_cube(pos, dir, clockwise, all)
 			-- -X = +Y or -Y
 			matchtiles[4] = oldtiles[clockwise and 2 or 1]..''
 		end
-		print('clockwise '..(clockwise and 1 or 0))
-		print(loadcubelet.node.param2..' '..loadcubelet.node.name)
-		print('oldtiles'..dump(oldtiles))
-		print('matchtiles '..dump(matchtiles))
 		
+		--get new cubelet
 		name, param2 = match_cubelet(matchtiles)
-
-		print(name..' '..param2)
-
-		--loadcubelet.node.param2 = (loadcubelet.node.param2+1)%4
+		
+		--place it
 		minetest.env:add_node(pos2, {name = name, param2 = param2})
 		local meta = minetest.env:get_meta(pos2)
 		meta:from_table(loadcubelet.meta)
 
 	end
 	end
-	print('done rotating')
 end
 
-function color_to_texture(color)
-	texture = ''
-	for t = 1, 7 do
-		texture = textures[t]
-		if color == colors[t] then
-			return texture
-		end
-	end
-end
-
-function register_cubelets(cubelet)
-	lettex = {}
-	for n = 1, 6 do
-		lettex[n] = color_to_texture(cubelet[n])
-	end
-	tiles = {unpack(lettex)}
-	
+function register_cubelets()
+	tiles = {unpack(textures)}
 	direction = true
 	cubelettiles = {}
 	for rotations = 1, 6 do
@@ -265,7 +252,7 @@ function register_cubelets(cubelet)
 				local string = oldmeta.fields.cube_center
 				if string ~= nil then
 					pos = minetest.string_to_pos(string)
-					spawn_cube(pos, false)
+					expand_cube(pos, false)
 				end
 			end,
 			drop = 'rubiks:cube',
@@ -308,7 +295,8 @@ function register_cubelets(cubelet)
 			tiles[4] = placeholder
 		end
 		direction = not direction
+		--rotating x, then z alternating will cause each color to be on the top once.
+		--Then adding facedir (y rotation) will get the rest of the rotations.
 	end
-end
-register_cubelets(colors)
+end register_cubelets()
 
